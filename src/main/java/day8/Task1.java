7d8e8aa53b4bf486174d004fc5745a6c8aa96064
jazzboy65 <alexis.gordeeff@gmail.com>
2022-01-06 16:42:05 +0500

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,25 @@
 package day8;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        /*long startTime1 = System.currentTimeMillis();
 
+        int[] arr = new int[20001];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        System.out.println(Arrays.toString(arr));
+        long stopTime1 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime1 - startTime1));*/
+        StringBuilder sb = new StringBuilder();
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            sb.append(i).append(",");
+        }
+        System.out.println(sb);
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime2 - startTime2));
     }
 }
