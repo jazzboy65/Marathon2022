3eb9fcae9ff733fc031db4bbabd15fdc748502ce
jazzboy65 <alexis.gordeeff@gmail.com>
2022-03-19 20:50:07 +0500

    complete day19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,44 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("src/main/resources/dushi.txt");
+        List<String> list = new ArrayList<>();
 
+        try {
+            Scanner sc = new Scanner(file);
+            sc.useDelimiter("[.,:;()?!<>\"\\s–]+");
+            while (sc.hasNext()) {
+                list.add(sc.next());
+            }
+
+            Map<String, Long> map = list.stream().collect(Collectors.groupingBy(it -> it, Collectors.counting()));
+            TreeMap<Long, String> revertedMap = new TreeMap<>(Comparator.reverseOrder());
+            for (String key : map.keySet()) {
+                revertedMap.put(map.get(key), key);
+            }
+
+            int i = 0;
+            for (Long key : revertedMap.keySet()) {
+                System.out.println(revertedMap.get(key) + " : " + key);
+                i++;
+
+                if (i == 100) {
+                    break;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,10 +1,40 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("src/main/resources/taxi_cars.txt");
+        HashMap<Integer, Point> hashMap = new HashMap<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                hashMap.put(sc.nextInt(), new Point(sc.nextInt(), sc.nextInt()));
+            }
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Введите координаты x1,y1,x2,y2");
+            int x1 = input.nextInt(),y1 = input.nextInt(),x2 = input.nextInt(),y2 = input.nextInt();
+
+            for(Map.Entry<Integer, Point> coordinate: hashMap.entrySet()) {
+                if(coordinate.getValue().getX() >= x1 && coordinate.getValue().getX() <= x2
+                && coordinate.getValue().getY() >= y1 && coordinate.getValue().getY() <= y2) {
+
+                    System.out.print(coordinate.getKey() + " ");
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
